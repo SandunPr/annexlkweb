7e60a120b218e54978dff0155ad9ba7c765bd44d
prabhashwara <prabhashwaraekanayake@gmail.com>
2026-07-29 18:53:11 +0530

    done

--- backup/annexlk-api/src/routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const searchController = require('../controllers/searchController');
-
-const router = express.Router();
-
-// Search is a public endpoint
-router.get('/', searchController.search);
-
-module.exports = router;
